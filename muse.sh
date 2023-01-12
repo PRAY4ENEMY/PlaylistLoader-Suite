db3f8a6ade821027cb2da858fbd9a9cb16282cca
PRAY4ENEMY <111301886+PRAY4ENEMY@users.noreply.github.com>
2023-01-11 22:53:16 -0500

    Update muse.sh

--- muse.sh
+++ muse.sh
@@ -2,16 +2,16 @@
 
 ls ~/Music/ > ~/Music/m.txt
 
-eval $(awk '{print "var"NR"="$1}' m.txt)
+eval $(awk '{print "var"NR"="$1}' ~/Music/m.txt)
 
-choice=$(cat m.txt) 
+choice=$(cat ~/Music/m.txt) 
 
 chosen=$(echo -e "$choice" | dmenu -i -p "Choose a Playlist")
 
 case "$chosen" in 
-	$var1) amberol ~/Music/$var1;;
-	$var2) amberol ~/Music/$var2;;
-	$var3) amberol ~/Music/$var3;;
-	$var4) amberol ~/Music/$var4;;
-	$var5) amberol ~/Music/$var5;;
+	$var1) setsid -f amberol ~/Music/$var1;;
+	$var2) setsid -f amberol ~/Music/$var2;;
+	$var3) setsid -f amberol ~/Music/$var3;;
+	$var4) setsid -f amberol ~/Music/$var4;;
+	$var5) setsid -f amberol ~/Music/$var5;;
 esac	
